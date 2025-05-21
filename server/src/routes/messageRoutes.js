7e381164dcf3e15cb54b7bfa9922b6d902d38a60
FarhanDevelopeir <fb4459467@gmail.com>
2025-05-21 10:27:53 +0500

    Removed server folder from tracking and updated .gitignore

--- server/src/routes/messageRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const messageController = require('../controllers/messageController');
-
-router.get('/:sender/:receiver', messageController.getMessages);
-
-module.exports = router;
